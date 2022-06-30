a4d434af704f74395ce0b156e8ad9f525d9c0667
YellaReddy <m.yellareddy6@gmail.com>
2022-06-30 09:41:10 +0530

    updated insertion sort algorithm

--- src/sortingAlgorithms/insertionSort.js
+++ src/sortingAlgorithms/insertionSort.js
@@ -1,37 +1,14 @@
 async function* insertionSort(array, highlight, swap, markSorted) {
-    // for (let i = 0; i <= array.length; i++) {
-    //     for (let j = i - 1; j >= 0; j--) {
-    //         yield await highlight(j, j - 1)
-    //         if (array[j] < array[j - 1]) {
-    //             yield await swap(j, j - 1, array)
-    //         } else {
-    //             // breaking here, we know once we don'tencounter a big value in the left side
-    //             // it means we already have that sub array sorted.
-    //             break;
-    //         }
-    //     }
-    //     markSorted(i);
-    //     yield;
-    // }
-
     for (let i = 0; i < array.length; i++) {
-        let keyIndex = i;
-        for (var j = i - 1; j >= 0; j--) {
-          yield await highlight(keyIndex, j);
-    
-          if (array[j] > array[keyIndex]) {
-            yield await swap(j, keyIndex, array);
-            keyIndex = j;
-          } else {
-            yield;
-            break;
-          }
+        let j = i;
+        yield await highlight(j, j-1)
+        while(j >=0 && array[j] < array[j - 1]) {
+          yield await swap(j, j - 1, array);
+          j -= 1;
         }
-    
         markSorted(i);
         yield;
-      }
+    }
 }
 
 export default insertionSort;
-
