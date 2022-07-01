8f18fde7368b22afa97b682401d476853fe090f2
YellaReddy <m.yellareddy6@gmail.com>
2022-07-01 16:46:52 +0530

    added random array generator function

--- src/shared/helper.js
+++ src/shared/helper.js
@@ -1,3 +1,12 @@
 export async function delay(time) {
     await new Promise(resolve => setTimeout(resolve, time))
 }
+
+export function generateRandomArray() {
+    const length = 6 + Math.floor(Math.random() * 10);
+    const array = [];
+    for (let i = 0; i < length; i++) {
+        array[i] = Math.floor(Math.random() * 999)
+    }
+    return array;
+}
