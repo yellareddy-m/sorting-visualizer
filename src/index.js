ba1b25a263d27c6dba0a517d7037fe6c540c0274
YellaReddy <m.yellareddy6@gmail.com>
2022-07-25 21:55:23 +0530

    removed strict mode

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ import { AppProvider } from './context/app.context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <React.StrictMode>
+    // <React.StrictMode>
         <AppProvider>
             <App />
         </AppProvider>
-    </React.StrictMode>
+    // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
