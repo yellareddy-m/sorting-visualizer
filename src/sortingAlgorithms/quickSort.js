d25c5cfa4aae3f409bfb5fa0e815d5777e49260b
YellaReddy <m.yellareddy6@gmail.com>
2022-07-26 10:50:34 +0530

    removed usage of generator function inside another generator function

--- src/sortingAlgorithms/quickSort.js
+++ src/sortingAlgorithms/quickSort.js
@@ -2,7 +2,7 @@ import { delay } from "../shared/helper";
 
 async function* quickSort(array, highlight, swap, markSorted, markPivot, markLessThanPivot, markMoreThanPivot, left = 0, right = array.length - 1,) {
     if (left < right) {
-        let pivotIdx = yield* await partition(array, left, right, highlight, swap, markSorted, markPivot, markLessThanPivot, markMoreThanPivot);
+        let pivotIdx = await partition(array, left, right, highlight, swap, markSorted, markPivot, markLessThanPivot, markMoreThanPivot);
         yield* await quickSort(array, highlight, swap, markSorted, markPivot, markLessThanPivot, markMoreThanPivot, left, pivotIdx - 1,);
         yield* await quickSort(array, highlight, swap, markSorted, markPivot, markLessThanPivot, markMoreThanPivot, pivotIdx + 1, right,);
     } else if (left === right) {
@@ -12,30 +12,30 @@ async function* quickSort(array, highlight, swap, markSorted, markPivot, markLes
     
 }
 
-async function* partition(array, start, end, highlight, swap, markSorted, markPivot, markLessThanPivot, markMoreThanPivot,) {
+async function partition(array, start, end, highlight, swap, markSorted, markPivot, markLessThanPivot, markMoreThanPivot,) {
     let pivot = array[start];
     // pivot index is start
     let swapIdx = start;
     delay(100);
     markPivot(start);
-    yield await markLessThanPivot(-1, true);
+    await markLessThanPivot(-1, true);
     // yield await markMoreThanPivot(-1, true);
     for (let i = start + 1; i <= end; i++) {
-        yield await highlight(start, i)
+        await highlight(start, i)
         if (pivot > array[i]) {
             // these numbers are less than pivot
             swapIdx++;
             if (swapIdx !== i) {
-                yield await swap(swapIdx, i, array);
+                await swap(swapIdx, i, array);
             }
             // if (swapIdx !== array.length - 1)
-            yield markLessThanPivot(swapIdx);
+            markLessThanPivot(swapIdx);
         } else {
             // these are greater than pivot
             // yield markMoreThanPivot(i);
         }
     }
-    yield await swap(start, swapIdx, array);
+    await swap(start, swapIdx, array);
     markSorted(swapIdx);
     markPivot(-1);
     return swapIdx;
